f732e1b0b0c5a738a3875441968cea7559197ae7
Calum Walsh <cazzlewazzle89@gmail.com>
2023-06-12 19:00:33 +1000

    Update make_feature_table.py

--- make_feature_table.py
+++ make_feature_table.py
@@ -4,7 +4,7 @@ import pandas as pd
 def make_feature_table(input_file, output_file, min_count, min_abundance):
     df = pd.read_csv(args.input_file, sep = "\t", header = None)
 
-    clusters = df[df[0] == "C"][["V9", "V3"]]
+    clusters = df[df[0] == "C"][[8, 2]]
     clusters.columns = ["Cluster", "Count"]
 
     clusters = clusters[clusters["Count"] >= args.min_count]
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input_file", dest = 'input_file', required = True, help = "Input file")
     parser.add_argument("-o", "--output_file", dest = 'output_file', required = True, help = "Output file")
     parser.add_argument("-m", "--min_count", dest = 'min_count', type = int, default = 2, help = "Minimum time each unique sequence must be detected to be included in relative abundance estimation. Sequences detected less frequently are discarded. Default value is 2 to discard singletons.")
-    parser.add_argument("-a", "--min_abundance", dest = 'min_abundance', type = float, default = 0.1, help = "Minimum relative abundance (%) of a sequence to be reported in feature table. Default is 0.1")
+    parser.add_argument("-a", "--min_abundance", dest = 'min_abundance', type = float, default = 0.1, help = "Minimum relative abundance of a sequence to be reported in feature table. Default is 0.1")
     args = parser.parse_args()
+
     make_feature_table(args.input_file, args.output_file, args.min_count, args.min_abundance)

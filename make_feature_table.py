13b6504048c5edec94ad428a07d7c20532cb9042
Calum Walsh <cazzlewazzle89@gmail.com>
2023-06-12 18:51:54 +1000

    Update make_feature_table.py

--- make_feature_table.py
+++ make_feature_table.py
@@ -14,7 +14,7 @@ def make_feature_table(input_file, output_file, min_count, min_abundance):
     clusters.to_csv(args.output_file, sep="\t")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description = "Make a feature table from vsearch culstering output")
     parser.add_argument("-i", "--input_file", dest = 'input_file', required = True, help = "Input file")
     parser.add_argument("-o", "--output_file", dest = 'output_file', required = True, help = "Output file")
     parser.add_argument("-m", "--min_count", dest = 'min_count', type = int, default = 2, help = "Minimum time each unique sequence must be detected to be included in relative abundance estimation. Sequences detected less frequently are discarded. Default value is 2 to discard singletons.")

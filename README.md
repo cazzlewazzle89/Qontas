fa8e9e70c585c93e14b972f332fed3a595085cef
Calum Walsh <cazzlewazzle89@gmail.com>
2026-02-09 13:17:54 +1100

    added outdir

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Clone repo and make scripts executable
 ```bash
 git clone https://github.com/cazzlewazzle89/Qontas.git ~/Software/QONTAS
 
-chmod +x QONTAS/*
+chmod +x ~/Software/QONTAS/*
 ```
 
 Create conda environment
@@ -40,13 +40,14 @@ Namely:
 1. Input FASTQ
 2. Input reference FASTA
 3. Output basename
-4. Minimum read length for FASTQ filtering
-5. Maximum read length for FASTQ filtering
-6. Amount of times a sequence must be observed per sample to be retained for relative abundance calculation (recommended to set this >1 to remove singletons [highly likely to be PCR artifacts or sequencing errors])
-7. Mimimum relative abundance as a percentage (eg. 2 or 0.1) for a sequence to be reported 
-8. Number of threads to use for minimap2 mapping  
+4. Output directory
+5. Minimum read length for FASTQ filtering
+6. Maximum read length for FASTQ filtering
+7. Amount of times a sequence must be observed per sample to be retained for relative abundance calculation (recommended to set this >1 to remove singletons [highly likely to be PCR artifacts or sequencing errors])
+8. Mimimum relative abundance as a percentage (eg. 2 or 0.1) for a sequence to be reported 
+9. Number of threads to use for minimap2 mapping  
 
-eg. `qontas.sh sample.fastq.gz ref.fa sample 600 650 2 0.1 10`  
+eg. `qontas.sh sample.fastq.gz ref.fa Qontas_Out sample 600 650 10 1 12`  
 
 `qontas` will print these values to screen and wait 10 seconds before running to give you a chance to cancel if anything is wrong  
 
@@ -54,7 +55,6 @@ eg. `qontas.sh sample.fastq.gz ref.fa sample 600 650 2 0.1 10`
 - [ ] automate pipeline in (better) single executable
 - [ ] create test dataset  
 - [ ] give option to retain or detele temp files
-- [ ] give option to specify output directory
 - [ ] modify to accept a list of input FASTQ files (TSV format) and output a single merged feature table  
    * alternatively, write a script that combines all the individual outputs 
    * Will need to modify to generate md5 read names so that they are groupable between samples  
--- qontas.sh
+++ qontas.sh
@@ -4,27 +4,29 @@ echo ''
 INPUTREADS=$1
 REFERENCE=$2
 BASENAME=$3
-FILT_MINLEN=$4
-FILT_MAXLEN=$5
-FEAT_MINCOUNT=$6
-FEAT_MINABUND=$7
-THREADS=$8
+OUTDIR=$4
+FILT_MINLEN=$5
+FILT_MAXLEN=$6
+FEAT_MINCOUNT=$7
+FEAT_MINABUND=$8
+THREADS=$9
 
 echo 'Input FASTQ is' $1
 echo 'Reference FASTA is' $2
 echo 'Output basename is' $3
-echo 'FASTQ reads shorter than' $4 'bp will be discarded'
-echo 'FASTQ reads longer than' $5 'bp will be discarded'
-echo 'Sequences observed fewer than' $6 'times per sample will be discarded BEFORE calculating relative abundance'
-echo 'Sequences with per-sample relative abundance below' $7 'will be discarded'
-echo 'Using' $8 'threads for minimap2 mapping'
+echo 'Output directory is' $4
+echo 'FASTQ reads shorter than' $5 'bp will be discarded'
+echo 'FASTQ reads longer than' $6 'bp will be discarded'
+echo 'Sequences observed fewer than' $7 'times per sample will be discarded BEFORE calculating relative abundance'
+echo 'Sequences with per-sample relative abundance below' $8 'will be discarded'
+echo 'Using' $9 'threads for minimap2 mapping'
 echo ''
 
 sleep 10
 
 filter_fastq.py \
     --input_file "$INPUTREADS" \
-    --output_file "$BASENAME"_filtered.fastq.gz \
+    --output_file "$OUTDIR"/"$BASENAME"_filtered.fastq.gz \
     --min_length "$FILT_MINLEN" \
     --max_length "$FILT_MAXLEN"
 
@@ -32,29 +34,33 @@ minimap2 \
     -a \
     -x map-ont \
     "$REFERENCE" \
-    "$BASENAME"_filtered.fastq.gz | samtools \
+    "$OUTDIR"/"$BASENAME"_filtered.fastq.gz | samtools \
         sort | samtools \
             view \
             -b \
-            -F 2308 > "$BASENAME".bam
+            -F 2308 > "$OUTDIR"/"$BASENAME".bam
 
-samtools index "$BASENAME".bam
+samtools index "$OUTDIR"/"$BASENAME".bam
 
 write_forward_reads.py \
-    --input_bam "$BASENAME".bam \
-    --output_fasta "$BASENAME".fasta
+    --input_bam "$OUTDIR"/"$BASENAME".bam \
+    --output_fasta "$OUTDIR"/"$BASENAME".fasta
 
 vsearch \
-    --derep_fulllength "$BASENAME".fasta \
-    --output "$BASENAME"_derep.fasta \
-    --uc "$BASENAME"_derep.txt
+    --derep_fulllength "$OUTDIR"/"$BASENAME".fasta \
+    --output "$OUTDIR"/"$BASENAME"_derep.fasta \
+    --uc "$OUTDIR"/"$BASENAME"_derep.txt
 
 make_feature_table.py \
-    -i "$BASENAME"_derep.txt \
-    -o "$BASENAME" \
+    -i "$OUTDIR"/"$BASENAME"_derep.txt \
+    -o "$OUTDIR"/"$BASENAME" \
     --min_count "$FEAT_MINCOUNT" \
     --min_abundance "$FEAT_MINABUND"
 
-seqkit grep -f "$BASENAME"_readnames.txt "$BASENAME".fasta > "$BASENAME"_sequences.fasta
+seqkit grep -f "$OUTDIR"/"$BASENAME"_readnames.txt "$OUTDIR"/"$BASENAME".fasta > "$OUTDIR"/"$BASENAME"_sequences.fasta
 
-rm -f "$BASENAME"_filtered.fastq.gz "$BASENAME".bam "$BASENAME".bam.bai "$BASENAME".fasta "$BASENAME"_derep.fasta "$BASENAME"_derep.txt "$BASENAME"_readnames.txt
+rm -f "$OUTDIR"/"$BASENAME"_filtered.fastq.gz \
+    "$OUTDIR"/"$BASENAME".bam "$OUTDIR"/"$BASENAME".bam.bai \
+    "$OUTDIR"/"$BASENAME".fasta \
+    "$OUTDIR"/"$BASENAME"_derep.fasta "$OUTDIR"/"$BASENAME"_derep.txt \
+    "$OUTDIR"/"$BASENAME"_readnames.txt
